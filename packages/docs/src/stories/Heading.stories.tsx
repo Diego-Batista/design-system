341c962dac82778364a2272522b9a8e345fcac14
Diego-Batista <diegobtistadev@gmail.com>
2022-12-04 13:49:52 -0300

    fixing error text component

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -10,17 +10,7 @@ export default {
   },
   argTypes: {
     size: {
-      options: [
-        '$xxs',
-        '$xs',
-        '$sm',
-        '$md',
-        '$lg',
-        '$2xl',
-        '$4xl',
-        '$5xl',
-        '$6xl',
-      ],
+      options: ['xxs', 'xs', 'sm', 'md', 'lg', '2xl', '4xl', '5xl', '6xl'],
       control: {
         type: 'inline-radio',
       },
